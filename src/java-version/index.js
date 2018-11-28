3e68baa1dfb54f598cb990326ff2849002093560
David Alexandre Marques dos Santos <dsantos@pentaho.com>
2018-11-28 22:26:19 +0000

    created 'java-version' script

---
+++ src/java-version/index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import { exec, echo } from 'shelljs'
+const silent = true
+
+exec('java -version 2>&1', { silent }, (code, output) => {
+  const isErrorCode = code !== 0
+  if (isErrorCode) {
+    return echo(`[ERROR] ${ output }`)
+  }
+
+  const isOpenJDK = output.includes('OpenJDK')
+
+  const [ versionLine ] = output.split('\n')
+  const [ /*java*/, /*version*/, javaVersion ] = versionLine.split(' ')
+
+  if (isOpenJDK) {
+    echo(`[INFO] You are using OpenJDK. Version: ${ javaVersion }`)
+  } else {
+    echo(`[INFO] You are using OracleJDK. Version: ${ javaVersion }`)
+  }
+})
